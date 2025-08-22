937d01bd5b8b8eed89e5bf410b13f3a586729e0a
minna <minnao27@ausdk12.org>
2025-08-22 13:45:47 -0700

    friday

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,21 @@
+public class BodyOfWater {
+    private String name;
+    private int largestDiameter;
+    private int aveDepth;
+    private String isSaltWater;
 
+    public BodyOfWater(String n, int l, int d, String s){
+        name = n;
+        largestDiameter = l;
+        aveDepth = d;
+        isSaltWater = s;
+    }
+
+    public void name(){
+        System.out.println("My name is: " + name );
+    }
+
+    public void largestDiameter(){
+        System.out.println("The largest diameter is: " + largestDiameter);
+    }
+}
